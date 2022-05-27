d075075f8b4390cb31d4bed9ed3bac1731cf9593
Niranjan <84449145+CodeWithNiranjan@users.noreply.github.com>
2022-05-27 14:10:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "slider",
+    slidesPerView: 3,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
